cc61a2459eed2368f00aa880153f9c4302f445f7
ikotan <mitsuharu.ikota@gmail.com>
2019-06-28 00:54:15 +0000

    リファクタリング

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-    "github.com/ikotan/go-todo-template/src/controller"
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
+
+	"github.com/ikotan/go-todo-template/src/controller"
 )
 
 func main() {
-    router := gin.Default()
+	router := gin.Default()
 
-    router.GET("/", controller.Index)
-    router.Run(":3001")
+	router.GET("/", controller.Index)
+	router.Run(":3001")
 }
